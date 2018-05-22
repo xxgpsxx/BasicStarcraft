de30831bb16ba3a9c186f47d6d41f7fe9b277452
xxgpsxx <gps360.infinityx@gmail.com>
2018-05-22 10:37:16 -0400

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -1,16 +1,104 @@
 import java.util.ArrayList;
-public class Runner
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.*;
+import javafx.scene.media.AudioClip;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.event.*;
+import javafx.scene.input.*;
+import javafx.scene.text.*;
+
+public class Runner extends Application implements EventHandler<InputEvent>
 {
-	public Runner()
+	GraphicsContext gc;
+	Image trooper;
+	int x = 0;
+	int y = 0;
+	int[] canvasSize = {700, 700};
+	AnimateObjects animate;
+	Map map;
+	Color[] color = {Color.BLUE, Color.RED, Color.YELLOW};
+	int players = 0;
+
+	public static void main(String[] args)
 	{
-		Map map = new Map();
+		launch();
+	}
+	public void start(Stage stage)
+	{
+		stage.setTitle("Basic Starcraft");
+		Group root = new Group();
+
+		Canvas canvas = new Canvas(canvasSize[0], canvasSize[1]);
+		root.getChildren().add(canvas);
+
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		scene.addEventHandler(KeyEvent.KEY_PRESSED,this);
+		scene.addEventHandler(MouseEvent.MOUSE_CLICKED, this);
+
+		gc = canvas.getGraphicsContext2D();
+		gc.setFill(Color.GREEN);
+		gc.fillRect(0, 0, canvasSize[0], canvasSize[1]);
+
+		map = new Map();
 		map.add(new Player(1, map));
 		map.add(new Player(2, map));
-		map.add(new Building(1000, 3, new Location(200, 200), 1, 5, 5));
-		map.add(new Building(1000, 3, new Location(800, 200), 2, 5, 5));
+
+		players = map.numPlayers();
+
+		map.add(new MainBuilding(map, new Location(200, 200), 1));
+		map.add(new MainBuilding(map, new Location(500, 500), 2));
+
+		gc.setFill(Color.BLACK);
+
+		animate = new AnimateObjects();
+		animate.start();
+
+		stage.show();
 	}
-	public static void main(String args[])
+	public class AnimateObjects extends AnimationTimer
 	{
-		Runner app = new Runner();
+		public void handle(long now)
+		{
+			for(int i = 0; i < players; i++)
+			{
+				gc.setFill(color[i]);
+				for(int j = 0; j < map.getPlayers().get(i).getBuildings().size(); j++)
+					gc.fillRect(
+						map.getPlayers().get(i).getBuildings().get(j).getLocation().getX(),
+						map.getPlayers().get(i).getBuildings().get(j).getLocation().getY(),
+						map.getPlayers().get(i).getBuildings().get(j).getWidth(),
+						map.getPlayers().get(i).getBuildings().get(j).getHeight());
+			}
+		}
+	}
+	public void handle(final InputEvent event)
+	{
+		if(event instanceof KeyEvent)
+		{
+			if(((KeyEvent)event).getCode() == KeyCode.LEFT || ((KeyEvent)event).getCode() == KeyCode.A)
+				x -= 5;
+			else if(((KeyEvent)event).getCode() == KeyCode.RIGHT || ((KeyEvent)event).getCode() == KeyCode.D)
+				x += 5;
+			else if(((KeyEvent)event).getCode() == KeyCode.UP || ((KeyEvent)event).getCode() == KeyCode.W)
+				y -= 5;
+			else if(((KeyEvent)event).getCode() == KeyCode.DOWN || ((KeyEvent)event).getCode() == KeyCode.S)
+				y += 5;
+		}
+		if(event instanceof MouseEvent)
+		{
+			System.out.println(((MouseEvent)event).getX());
+			System.out.println(((MouseEvent)event).getY());
+		}
 	}
 }
