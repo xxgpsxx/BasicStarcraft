ec6249247aebe3db2782c6569ae294fb468f5eb0
xxgpsxx <gps360.infinityx@gmail.com>
2018-05-31 10:41:55 -0400

    Update Util.java

--- Util.java
+++ Util.java
@@ -1,44 +1,62 @@
-import java.util.ArrayList;
-import javafx.animation.AnimationTimer;
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.image.*;
-import javafx.scene.Scene;
-import javafx.scene.SnapshotParameters;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.*;
-import javafx.scene.media.AudioClip;
-import java.net.URL;
-import javafx.application.Application;
-import javafx.geometry.Rectangle2D;
-import javafx.event.*;
-import javafx.scene.input.*;
-import javafx.scene.text.*;
-import javafx.geometry.Insets;
-
-public class Util
+public class Unit extends Thing
 {
-	public static Image rotate(Image image, double rotation)
+	private double speed = 0;
+	private int damage = 0;
+	private double radius = 0.0;
+	private boolean attacking = false;
+	private Location target;
+	private double direction = 90.0;
+	private boolean moving = false;
+	public Unit(Map map, int health, int armor, Location location, int owner, double speed, int damage, double radius)
 	{
-		ImageView iv = new ImageView(image);
-		iv.setRotate((int)rotation);
-		SnapshotParameters params = new SnapshotParameters();
-		params.setFill(Color.TRANSPARENT);
-		return iv.snapshot(params, null);
+		super(map, health, armor, location, owner);
+		this.speed = speed;
+		this.damage = damage;
+		this.radius = radius;
 	}
-	public static double asin(double ratio) { return Math.toDegrees(Math.asin(ratio)); }
-	public static double sin(double degrees) { return Math.toDegrees(Math.sin(Math.toRadians(degrees))); }
-	public static double cos(double degrees) { return Math.toDegrees(Math.cos(Math.toRadians(degrees))); }
-	public static double findCotermAngle(double degrees)
+	public Unit(Map map, int health, int armor, Location location, double speed, int damage, double radius)
 	{
-		if(degrees > 360)
-			return findCotermAngle(degrees - 360);
-		else if(degrees < 0)
-			return findCotermAngle(degrees + 360);
-		return degrees;
+		super(map, health, armor, location);
+		this.speed = speed;
+		this.damage = damage;
+		this.radius = radius;
 	}
+	public Location moveTo(Location target)
+	{
+		direction = this.getLocation().getAngleTo(target);
+		double distance = this.getLocation().distanceFrom(target);
+		double remainder = distance % speed;
+		double modified = distance - remainder;
+		target = this.getLocation().getLocationByDistance(
+			modified,
+			direction); //works
+		moving = true;
+		System.out.println(target);
+		System.out.println(direction);
+		return target;
+	}
+	public double setDirection(double x)
+	{
+		direction = x;
+		return x;
+	}
+	public Location setLocation(Location location)
+	{
+		this.teleportTo(location);
+		if(super.getLocation() == target)
+			moving = false;
+		if(this.getLocation() != null)
+			direction = this.getLocation().getAngleTo(target);
+		return location;
+	}
+	public double turn(double direction) { this.direction = direction; return direction; }
+	public boolean isMoving() { return moving; }
+	public double getDirection() { return direction; }
+	public Location getTarget() { return target; }
+	public Map getMap() { return super.getMap(); }
+	public boolean isAttacking() { return attacking; }
+	public double getRadius() { return radius; }
+	public double getSpeed() { return speed; }
+	public int getDamage() { return damage; }
 }
+
